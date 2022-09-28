067357760b800145b3ac96f9ac7659429c608ed8
WakHerv7 <wakam.hermann@gmail.com>
2022-09-28 14:27:43 +0100

    wakherv-commit-003

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,18 @@
 import {createStore} from 'vuex'
 import inboxModule from "@/store/modules/inbox.module";
-import archiveModule from "@/store/modules/archive.module";
 
 export default createStore({
     state: {
         inboxState: inboxModule.state,
-        archiveState: archiveModule.state,
     },
     mutations: {},
     actions: {},
     modules: {
         inboxModule,
-        archiveModule,
     },
     getters: {
         getInboxState: function (state) {
             return state.inboxState;
-        },
-        getArchiveState: function (state) {
-            return state.inboxState.archiveList;
-        },
+        }
     }
 })
